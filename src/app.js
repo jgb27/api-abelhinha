de26b56c33901faea4a3f87fbb17764254b7d4f5
João Gustavo Soares Bispo <jgbispo20@gmail.com>
2023-09-07 10:37:03 -0300

    Configurando o Cors

--- src/app.js
+++ src/app.js
@@ -3,7 +3,16 @@ import router from "./router.js";
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", 'http://localhost:5173');
+  res.setHeader("Access-Control-Allow-Methods", 'GET, POST, DELETE');
+  res.setHeader("Access-Control-Allow-Headers", 'X-Requested-With,content-type');
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+})
+
 app.use(express.json());
 app.use(router);
+
 
 export default app;
