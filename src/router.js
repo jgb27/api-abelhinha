107ce833829c1c196439f51ef9cbec6ed7714631
João Gustavo Soares Bispo <jgbispo20@gmail.com>
2023-09-11 13:42:29 -0300

    feat(controller): add product search
    Este commit introduz um novo controlador para aprimorar o recurso de pesquisa de produtos. Os usuários agora podem pesquisar produtos por nome ou tag, melhorando a usabilidade geral do aplicativo. Essa mudança inclui a adição de rotas e lógica de manipulação de dados para garantir uma experiência de pesquisa contínua e eficiente para os usuários.

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { AddNewProduct, DeleteProduct, GetAllProducts } from "./controller/ProductController.js";
+import { AddNewProduct, DeleteProduct, FindProductByName, FindProductByTag, GetAllProducts } from "./controller/ProductController.js";
 import { Login, Authenticate } from "./controller/AuthController.js";
 import { upload } from "./config/multer.js"
 import { CreateUser } from "./controller/UserController.js";
@@ -8,30 +8,28 @@ import jwt from 'jsonwebtoken';
 const router = express.Router();
 
 router.post("/login", Login);
+router.post("/register", CreateUser);
 router.get("/product", GetAllProducts);
+router.get("/product/name/:name", FindProductByName);
+router.get("/product/tag/:tag", FindProductByTag);
 
 router.post("/verify", (req, res) => {
   const { token } = req.body;
 
-  const verify = jwt.verify(token, process.env.AWS_SECRET_ACCESS_KEY, (error, decodedToken) => {
+  const verify = jwt.verify(token, process.env.AWS_SECRET_ACCESS_KEY, (error) => {
     if (error) {
       return res.status(401).json({ message: 'Token de autenticação inválido', data: false });
     } else {
-      return  res.status(200).json({ message: 'Token de autenticação inválido', data: true });
+      return res.status(200).json({ message: 'Token de autenticação inválido', data: true });
     }
   });
 
   return res.status(200).json({ token: verify })
 });
 
-router.post("/register", CreateUser);
 
 router.use(Authenticate);
 router.post("/product", upload.single("image"), AddNewProduct);
 router.delete("/product/:id", DeleteProduct);
-
-router.post("/access", (req, res) => {
-  res.status(200).send("Tudo certo");
-});
 
 export default router;
